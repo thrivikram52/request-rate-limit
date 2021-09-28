8a83ca6aed8f507a7fe1c023d28813ba6a04cea0
thrivikram52 <thrivikram52@gmail.com>
2021-09-29 00:45:19 +0530

    Add files via upload

--- rateLimiter.js
+++ rateLimiter.js
@@ -1,6 +1,11 @@
-const redis = require('async-redis')
+const util = require('util');
+const redis = require('redis');
 const redisClient = redis.createClient();
 const moment = require('moment')
+
+redisClient.get = util.promisify(redisClient.get);
+redisClient.set = util.promisify(redisClient.set);
+redisClient.expire = util.promisify(redisClient.expire);
 
 redisClient.on("error", function(error) {
     console.error("redis connection not working..." ,error);
@@ -13,7 +18,7 @@ let DURATION_IN_MIN;
 const rateLimitInitializer = (rate_limit_attribute,max_request_count,duration_in_min) => {
     RATE_LIMIT_ATTRIBUTE = rate_limit_attribute,
     MAX_REQUEST_COUNT = max_request_count;
-    DURATION_IN_MIN = duration_in_min;
+    DURATION_IN_MIN = duration_in_min;    
 };
 
 const rateLimitChecker = async(req,res,next) => {
@@ -21,21 +26,15 @@ const rateLimitChecker = async(req,res,next) => {
     let counterObj = await redisClient.get(cache_key);
     counterObj = JSON.parse(counterObj);
     if(counterObj) {
-        let currentTime = moment().unix()
-        let difference = (currentTime - counterObj.startTime)/60
-        if(difference >= DURATION_IN_MIN) {
-            //Reset counter
-            upsertCounter(cache_key, next);
-        } else {        
-          if(counterObj.count > MAX_REQUEST_COUNT-1) {
-            //Throttle request
-            res.status(429).send({"error": 1000, "message": "throttled limit exceeded..."});
-          } else {
-            //Update the counter
-            counterObj.count++
-            redisClient.set(cache_key,JSON.stringify(counterObj))
-            next()
-          }
+        if(counterObj.count > MAX_REQUEST_COUNT-1) {
+        //Throttle request
+        res.status(429).send({"error": 1000, "message": "throttled limit exceeded..."});
+        } else {
+        //Update the counter
+        counterObj.count++
+        await redisClient.set(cache_key,JSON.stringify(counterObj));
+        await redisClient.expire(cache_key, DURATION_IN_MIN*60);
+        next();             
         }
     } else {
         //Add new user
@@ -53,6 +52,7 @@ function upsertCounter(cache_key, next) {
         'count': 1,
         'startTime': moment().unix()
     };
-    redisClient.set(cache_key, JSON.stringify(counterObj)); 
+    redisClient.set(cache_key, JSON.stringify(counterObj),'EX', 10);
+    redisClient.expire(cache_key, DURATION_IN_MIN*60);
     next();
 }
